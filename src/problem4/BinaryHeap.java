ff75c34ad7b7f86c2eb523f1aefc66a30136385d
Djordje Dimitrov <dimitrovdjordje@gmail.com>
2023-11-05 16:29:55 +0100

    Added BinaryHeap class.

---
+++ src/problem4/BinaryHeap.java
@@ -0,0 +1,100 @@
+package problem4;
+
+import java.nio.BufferUnderflowException;
+
+public class BinaryHeap<AnyType extends Comparable<? super AnyType>> {
+
+    private int currentSize;
+
+    private static final int DEFAULT_CAPACITY = 10;
+
+    private AnyType[] array;
+
+    public BinaryHeap() {
+        this.currentSize = 0;
+    }
+
+    public BinaryHeap(AnyType[] array) {
+        this.currentSize = array.length;
+        // this.array should have 0 index empty, and the rest is the same as the array
+        this.array = (AnyType[]) new Comparable[(currentSize + 2) * 11 / 10];
+        int i = 1;
+        for (AnyType element : array) {
+            this.array[i++] = element;
+        }
+        buildHeap();
+    }
+
+
+    public void buildHeap() {
+        for (int i = currentSize / 2; i > 0; i--) {
+            percolateDown(i);
+        }
+    }
+
+    public AnyType findMin() {
+        if (array == null || array.length == 0) {
+            return null;
+        }
+        return array[1];
+    }
+
+    private boolean isEmpty() {
+        return currentSize == 0;
+    }
+
+
+    public AnyType deleteMin() {
+        if (isEmpty()) {
+            throw new BufferUnderflowException();
+        }
+        AnyType min = findMin();
+        array[1] = array[currentSize--];
+        percolateDown(1);
+        return min;
+    }
+
+
+    public void insert(AnyType element) {
+        if (currentSize == array.length - 1) {
+            enlargeArray(array.length * 2 + 1);
+        }
+        array[array.length - 1] = element;
+        percolateUp(element);
+    }
+
+    private void enlargeArray(int newSize) {
+        AnyType[] newArray = (AnyType[]) new Comparable[newSize];
+        System.arraycopy(array, 0, newArray, 0, array.length);
+        array = newArray;
+    }
+
+
+
+
+    private void percolateDown(int hole) {
+        int child;
+        AnyType tmp = array[hole];
+        for (; hole * 2 <= currentSize; hole = child) {
+            child = hole * 2;
+            if (child != currentSize && array[child + 1].compareTo(array[child]) < 0) {
+                child++;
+            }
+            if (array[child].compareTo(tmp) < 0) {
+                array[hole] = array[child];
+            } else {
+                break;
+            }
+        }
+        array[hole] = tmp;
+    }
+
+
+    private void percolateUp(AnyType x) {
+        int hole = ++currentSize;
+        for (array[0] = x; x.compareTo(array[hole/2]) < 0; hole /= 2) {
+            array[hole] = array[hole/2];
+        }
+        array[hole] = x;
+    }
+}
